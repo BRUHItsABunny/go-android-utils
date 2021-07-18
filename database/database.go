62fd2e05de61d9b5edd1fc71cc2f3ca2a4e4c9b8
BRUHItsABunny <323github@bruhitsabunny.uu.ma>
2021-07-19 01:23:08 +0200

    Add todo

--- database/database.go
+++ database/database.go
@@ -1,5 +1,5 @@
 package database
 
 // Here we store a few devices and way to get them, just easy access in case you want to prototype a few devices in a library fast
-
+// TODO: Actually add a few devices here...
 var DeviceDB = map[string]string{}
