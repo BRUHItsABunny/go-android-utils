bdb7a4b9fb2a547be01b40b749961182b4ae6355
BRUHItsABunny <323github@bruhitsabunny.uu.ma>
2021-08-07 10:27:55 +0200

    add Oneplus 5 to db

--- database/database.go
+++ database/database.go
@@ -11,7 +11,7 @@ import (
 // TODO: Actually add a few devices here...
 var DeviceDB = map[string]string{
 	// "oneplus3": "",
-	// "oneplus5": "",
+	"oneplus5":  "{\"android_id\":{\"id\":\"163f5f0f9a621d72\"},\"locale\":{\"language\":\"en\",\"country\":\"us\"},\"android_version\":{\"sdk\":28},\"device\":\"OnePlus5\",\"manufacturer\":\"OnePlus\",\"model\":\"ONEPLUS A5000\",\"product\":\"OnePlus5\",\"build\":\"PKQ1.180716.001\",\"type\":\"user\",\"tags\":\"release-keys\",\"rom_version\":\"2002242003\",\"dpi\":420,\"resolution_horizontal\":1080,\"resolution_vertical\":1920,\"architecture\":{\"architecture\":\"arm64\"},\"time_zone\":{\"location\":\"UTC\"}}",
 	"oneplus7t": "{\"android_id\":{\"id\":\"163f5f0f9a621d72\"},\"locale\":{\"language\":\"en\",\"country\":\"us\"},\"android_version\":{\"sdk\":29},\"device\":\"OnePlus7T\",\"manufacturer\":\"OnePlus\",\"model\":\"HD1905\",\"product\":\"OnePlus7T\",\"build\":\"QKQ1.190716.003\",\"type\":\"user\",\"tags\":\"release-keys\",\"rom_version\":\"2101212100\",\"dpi\":420,\"resolution_horizontal\":1080,\"resolution_vertical\":2400,\"architecture\":{\"architecture\":\"arm64\"},\"time_zone\":{\"location\":\"UTC\"}}",
 	// "oneplus9": "",
 	"oneplus9pro": "{\"android_id\":{\"id\":\"163f5f0f9a621d72\"},\"locale\":{\"language\":\"en\",\"country\":\"us\"},\"android_version\":{\"sdk\":30},\"device\":\"OnePlus9Pro\",\"manufacturer\":\"OnePlus\",\"model\":\"LE2125\",\"product\":\"OnePlus9Pro\",\"build\":\"RKQ1.201105.002\",\"type\":\"user\",\"tags\":\"release-keys\",\"rom_version\":\"2105290043\",\"dpi\":600,\"resolution_horizontal\":1440,\"resolution_vertical\":3216,\"architecture\":{\"architecture\":\"arm64\"},\"time_zone\":{\"location\":\"UTC\"}}",
